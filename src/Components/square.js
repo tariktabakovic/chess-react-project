f935438f70f8b0a34a12f32e4f5fa1176e9f67ca
tariktabakovic <tariktab97@gmail.com>
2020-02-19 10:50:28 -0500

    creates Square component, and imports SYMBOLS

---
+++ src/Components/square.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SYMBOLS} from '../PieceConstants/index';
+
+export default class Square extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default square
