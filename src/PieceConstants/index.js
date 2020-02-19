2a623ef0a775b0dfd7601660531dd3c722303ca2
tariktabakovic <tariktab97@gmail.com>
2020-02-19 10:08:23 -0500

    adds constants for game pieces

---
+++ src/PieceConstants/index.js
@@ -0,0 +1,16 @@
+// Black Pieces
+const king = 'k';
+const queen = 'q';
+const bishop = 'b';
+const knight = 'n';
+const rook = 'r';
+const pawn = 'p';
+
+// White Pieces
+const whiteKing = 'K';
+const whiteQueen = 'Q';
+const whiteBishop = 'B';
+const whiteKnight = 'N';
+const whiteRook = 'R';
+const whiteParn = 'P';
+
