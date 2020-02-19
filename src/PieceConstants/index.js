c6d5680ba44e7f00874280805aa0c7d0c7aa45c4
tariktabakovic <tariktab97@gmail.com>
2020-02-19 10:28:00 -0500

    adds unicode characters for each piece

--- src/PieceConstants/index.js
+++ src/PieceConstants/index.js
@@ -1,4 +1,4 @@
-// Black Pieces
+// Black Pieces:
 const king = 'k';
 const queen = 'q';
 const bishop = 'b';
@@ -6,11 +6,26 @@ const knight = 'n';
 const rook = 'r';
 const pawn = 'p';
 
-// White Pieces
+// White Pieces:
 const whiteKing = 'K';
 const whiteQueen = 'Q';
 const whiteBishop = 'B';
 const whiteKnight = 'N';
 const whiteRook = 'R';
-const whiteParn = 'P';
+const whitePawn = 'P';
 
+// Unicode Characters for Chess: 'fileformat.info'
+export const SYMBOLS = {
+    [king]: '\u265A',
+    [queen]: '\u265B',
+    [bishop]: '\u265C',
+    [knight]: '\u265D',
+    [rook]: '\u265E',
+    [pawn]: '\u265F',
+    [whiteKing]: '\u2654',
+    [whiteQueen]: '\u2655',
+    [whiteBishop]: '\u2657',
+    [whiteKnight]: '\u2658',
+    [whiteRook]: '\u2656',
+    [whitePawn]: '\u2659'
+}
