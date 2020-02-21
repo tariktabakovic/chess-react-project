7b81059980ffddb78df88fcf7cd6fb4e520c10b9
tariktabakovic <tariktab97@gmail.com>
2020-02-21 18:13:47 -0500

    creates class component for DeadPieces, renders square & mapping fallenSoldiers to newly render square

---
+++ src/Components/DeadPieces.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'src/App.css';
+import Square from './Square';
+
+export default class DeadPieces extends React.Component {
+    renderSquare (square, i, squareShade){
+        return <Square 
+        piece={square}
+        style= {square.style}
+        />
+    }
+    render(){
+        return(
+            <div>
+                <div className = 'board-row'>{this.props.whiteFallenSoldiers.map((ws,index) => this.renderSquare(ws,index))}
+                </div>
+                <div className = 'board-row'>{this.props.blackFallenSoldiers.map((bs, index) => this.renderSquare(bs,index))}
+                </div>
+            </div>
+        )
+    }
+}
