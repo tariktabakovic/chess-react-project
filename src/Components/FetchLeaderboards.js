52beac2ee17acbc3c6e79b72a3b7a38d5c5d70aa
tariktabakovic <tariktab97@gmail.com>
2020-02-23 15:07:33 -0500

    implements loading state and componentdidmount() for fetching api

--- src/Components/FetchLeaderboards.js
+++ src/Components/FetchLeaderboards.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export default class FetchLeaderBoards extends React.Component {
+
+    state = {
+        loading: true
+    }
+
+    componentDidMount() {
+        const url = 'https://api.chess.com/pub/leaderboards';
+        const response = fetch(url);
+    }
+    
     render () {
         return 
+        <div>
+            {this.state.loading ? <div>loading...</div> : <div>person..</div>}
+        </div>
     }
 }
