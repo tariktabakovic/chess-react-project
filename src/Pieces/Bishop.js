6bcdca13c7fc6d665e21eeb14da241e63aad1807
tariktabakovic <tariktab97@gmail.com>
2020-02-22 22:50:34 -0500

    extends Piece class for Bishop, implements functions isMovePossible & getSrcToDestPath

--- src/Pieces/Bishop.js
+++ src/Pieces/Bishop.js
@@ -1,7 +1,34 @@
 import Piece from './PieceClass';
-
+import SYMBOLS from './PieceConstants'
 export default class Bishop extends Piece {
     constructor(player){
-        
+        super(player, (player === 1? "\u2657": "\u265C"));
+    }
+    
+    isMovePossible(src, dest){
+        return (Math.abs(src-dest) % 9 === 0 || Math.abs(src - dest) % 7 === 0);
+    }
+    getSrcToDestPath(src, dest){
+        let path = [], pathStart, pathEnd, increamentBy;
+        if(src > dest){
+            pathStart = dest;
+            pathEnd = src;
+        }
+        else {
+            pathStart = src;
+            pathEnd = dest;
+        }
+        if(Math.abs(src - dest) & 9 ===0){
+            increamentBy = 9;
+            pathStart += 9;
+        }
+        else {
+            incrementBy = 7;
+            pathStart += 7;
+        }
+        for(let i = pathStart; i < pathEnd; i+=increamentBy){
+            path.push(i);
+        }
+        return path;
     }
 }
