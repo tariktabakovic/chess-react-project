c58308391e707d79dd30de63cc2d8c53b4293621
tariktabakovic <tariktab97@gmail.com>
2020-02-23 18:08:44 -0500

    displays json data for top 5 players from chess.com

--- src/Components/FetchLeaderboards.js
+++ src/Components/FetchLeaderboards.js
@@ -4,27 +4,100 @@ export default class FetchLeaderBoards extends React.Component {
 
     state = {
         loading: true,
-        rank: null
+        rank1: null,
+        rank2: null,
+        rank3: null,
+        rank4: null,
+        rank5: null
     }
 
     async componentDidMount() {
         const url = 'https://api.chess.com/pub/leaderboards';
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({rank: data.daily[0], loading: false});
-        console.log(data.daily);
+        this.setState({
+            rank1: data.daily[0], loading: false,
+            rank2: data.daily[1], loading: false,
+            rank3: data.daily[2], loading: false,
+            rank4: data.daily[3], loading: false,
+            rank5: data.daily[4], loading: false
+        });
+        
+        console.log(this.state.rank2);
+        console.log(this.state.rank1);
+        console.log(this.state.rank3);
+
     }
     
     render () {
         return (
         <div>
-            {this.state.loading || !this.state.rank? (
-            <div>loading...</div> 
-            ) : ( 
+
             <div>
-                <div>{this.state.rank.username}</div>
+                {this.state.loading || !this.state.rank1? (
+                    <div>loading...</div> 
+                    ) : ( 
+                        <div>
+                    <div>Username: {this.state.rank1.username}</div>
+                    <div>Rank: {this.state.rank1.rank}</div>
+                    <div> Avatar:
+                        <img src = {this.state.rank1.avatar}/>
+                    </div>
+                </div>
+                )})
             </div>
-            )}
+            <div>
+                {this.state.loading || !this.state.rank2? (
+                    <div>loading...</div>
+                    ) : (
+                        <div>
+                    <div>Username: {this.state.rank2.username}</div>
+                    <div>Rank: {this.state.rank2.rank}</div>
+                    <div> Avatar:
+                        <img src = {this.state.rank2.avatar}/>
+                    </div>
+                </div>
+                )})
+            </div>
+            <div>
+                {this.state.loading || !this.state.rank3? (
+                    <div>loading...</div>
+                    ) : (
+                        <div>
+                    <div>Username: {this.state.rank3.username}</div>
+                    <div>Rank: {this.state.rank3.rank}</div>
+                    <div> Avatar:
+                        <img src = {this.state.rank3.avatar}/>
+                    </div>
+                </div>
+                )})
+            </div>
+            <div>
+                {this.state.loading || !this.state.rank4? (
+                    <div>loading...</div>
+                    ) : (
+                        <div>
+                    <div>Username: {this.state.rank4.username}</div>
+                    <div>Rank: {this.state.rank4.rank}</div>
+                    <div> Avatar:
+                        <img src = {this.state.rank4.avatar}/>
+                    </div>
+                </div>
+                )})
+            </div>
+            <div>
+                {this.state.loading || !this.state.rank5? (
+                    <div>loading...</div>
+                    ) : (
+                        <div>
+                    <div>Username: {this.state.rank5.username}</div>
+                    <div>Rank: {this.state.rank5.rank}</div>
+                    <div> Avatar:
+                        <img src = {this.state.rank5.avatar}/>
+                    </div>
+                </div>
+                )})
+            </div>
         </div>
         )
     }
