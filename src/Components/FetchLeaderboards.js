8b3c56a483208fdce9d985335991bdf8f00b99d3
tariktabakovic <tariktab97@gmail.com>
2020-02-23 15:24:26 -0500

    renders username of player thats ranked #1

--- src/Components/FetchLeaderboards.js
+++ src/Components/FetchLeaderboards.js
@@ -11,14 +11,20 @@ export default class FetchLeaderBoards extends React.Component {
         const url = 'https://api.chess.com/pub/leaderboards';
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({rank: data.daily.rank[0]})
+        this.setState({rank: data.daily[0], loading: false});
         console.log(data.daily);
     }
     
     render () {
         return (
         <div>
-            {this.state.loading ? <div>loading...</div> : <div></div>}
+            {this.state.loading || !this.state.rank? (
+            <div>loading...</div> 
+            ) : ( 
+            <div>
+                <div>{this.state.rank.username}</div>
+            </div>
+            )}
         </div>
         )
     }
