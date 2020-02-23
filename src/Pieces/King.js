1ae70d19d00704bffbace1bb54195e7b6d9e5418
tariktabakovic <tariktab97@gmail.com>
2020-02-23 14:53:52 -0500

    removes symbol import

--- src/Pieces/King.js
+++ src/Pieces/King.js
@@ -1,18 +1,17 @@
 import Piece from '/Users/tariktabakovic/chess-react-project/src/Pieces/PieceClass.js';
-import SYMBOLS from './PieceConstants';
 export default class King extends Piece {
     constructor (player){
         super(player, (player ===1 ? 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg' : 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg'));
     }
     isMovePossible(src, dest){
         return (
-            src - 9 ===dest ||
+            src - 9 === dest ||
             src - 8 === dest ||
             src - 7 === dest ||
-            src + 1 ===dest ||
-            src + 9 ===dest ||
-            src + 8 ===dest ||
-            src - 1 ===dest  );
+            src + 1 === dest ||
+            src + 9 === dest ||
+            src + 8 === dest ||
+            src - 1 === dest  );
     }
     getSrcToDestPath(src, dest){
         return [];
