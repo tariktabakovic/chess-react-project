6e146e0d30fd36d6458deda352b9baf4b520e3fc
tariktabakovic <tariktab97@gmail.com>
2020-02-22 23:25:21 -0500

    imports all of the piece classes

---
+++ src/InitialBoard.js
@@ -0,0 +1,6 @@
+import Pawn from './Pieces/Pawn';
+import Knight from './Pieces/Knight';
+import Bishop from './Pieces/Bishop';
+import Rook from './Pieces/Rook';
+import Queen from './Pieces/Queen';
+import King from './Pieces/King';
