0d13c04b1bec72ce312475c5bd20134940c93e93
tariktabakovic <tariktab97@gmail.com>
2020-02-22 23:30:36 -0500

    creates initialBoard function & creates an array of 64 positions

--- src/InitialBoard.js
+++ src/InitialBoard.js
@@ -4,3 +4,15 @@ import Bishop from './Pieces/Bishop';
 import Rook from './Pieces/Rook';
 import Queen from './Pieces/Queen';
 import King from './Pieces/King';
+
+export default function initialBoard(){
+    const squares = Array(64).fill(null);
+    for(let i = 8; i < 16; i++){
+        squares[i] = new Pawn(2);
+        squares[i+40] = new Pawn(1);
+    }
+    squares [0] = new Rook (2);
+    squares [7] = new Rook (2);
+    squares [56] = new Rook(1);
+    squares [63] = new Rook (1);
+}
