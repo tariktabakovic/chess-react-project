863ace4ff20bbb0f49cbfae9a1cfc04f4254ce4c
tariktabakovic <tariktab97@gmail.com>
2020-02-22 23:03:13 -0500

    implements initialPositions for pawn piece

--- src/Pieces/Pawn.js
+++ src/Pieces/Pawn.js
@@ -4,5 +4,9 @@ import SYMBOLS from './PieceConstants';
 export default class Pawn extends Piece {
     constructor(player){
         super(player, (player === 1? '\u2659' : '\u265F'))
+        this.initialPositions = {
+            1: [48, 49, 50, 51, 52, 53, 54, 55],
+            2: [8, 9, 10, 11, 12, 13, 14, 15]
+        }
     }
 }
